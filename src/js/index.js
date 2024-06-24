47a663c38dad6292d113a865905e42dbd26d6d68
Luisa Saenz <luisasaenz@Astromac.local>
2024-06-24 15:39:43 -0500

    component start

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const containerCard = document.getElementById("container-card");
+
+function productStartCard(products) {
+  products.forEach((product) => {
+    const productNew = document.createElement("div");
+    productNew.classList = ("card-products");
+
+    const productImage = document.createElement("img");
+    productImage.classList = ("image-products");
+    productImage.src = product.imag;
+    productImage.alt = product.name;
+
+    const productName = document.createElement("h2")
+    productName.textContent = product.name
+
+    const productPrice = document.createElement("p")
+    productPrice.textContent = `$${product.price.toLocaleString()}`;
+
+    const buyButton = document.createElement("button")
+    buyButton.textContent = "Aregar al carrito"
+    buyButton.classList.add = ("buy-button");
+
+
+    productNew.appendChild(productImage);
+    productNew.appendChild(productName);
+    productNew.appendChild(productPrice);
+    productNew.appendChild(buyButton);
+
+    containerCard.appendChild(productNew);
+  });
+}
+
+productStartCard(products);
---
+++ src/js/products.js
@@ -0,0 +1,38 @@
+const products = [
+  {
+    id: 1,
+    name: "Lirio",
+    imag: "./img/products/lirio.jpg",
+    price: 50000,
+  },
+  {
+    id: 2,
+    name: "LunaAzul",
+    imag: "./img/products/lunaAzul.jpg",
+    price: 60000,
+  },
+  {
+    id: 3,
+    name: "Ruby",
+    imag: "./img/products/ruby.jpg",
+    price: 70000,
+  },
+  {
+    id: 4,
+    name: "Alejandrina",
+    imag: "./img/products/alejandrina.jpg",
+    price: 80000,
+  },
+  {
+    id: 5,
+    name: "Duna",
+    imag: "./img/products/duna.jpg",
+    price: 90000,
+  },
+  {
+    id: 6,
+    name: "Esmeralda",
+    imag: "./img/products/esmeralda.jpg",
+    price: 100000,
+  }
+];
